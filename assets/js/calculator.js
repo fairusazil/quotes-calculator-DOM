8ebeaf4016c3838c7f927c7a30157fdf8560bf7e
fairusazil <fairus.azil@gmail.com>
2020-04-09 04:44:58 +0700

    revisi

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -3,7 +3,7 @@ function calculation() {
     let input1 = parseInt(document.getElementById("input1").value);
     let input2 = parseInt(document.getElementById("input2").value);
     let operand = document.getElementById("operand").value;
-    let resultOperand = document.getElementById("result");
+    let resultop = document.getElementById("result");
 
     if (isNaN(input1) || isNaN(input2)) {
         alert("Jangan kosong bung, isi pakai angka!!")
@@ -16,7 +16,9 @@ function calculation() {
             total = input1 * input2;
         } else if (operand == "/") {
             total = input1 / input2;
+        } else if (operand == "%") {
+            total = input1 % input2;
         }
     }
-    resultOperand.value = total;
+    resultop.value = total;
 }
